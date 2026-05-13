e42c07bac1f4bd1a76f0bd8a32da1d543dfa0a13
Yui <Pielord.home@gmail.com>
2026-05-13 14:52:16 +0100

    +3 solutions

--- Template/obj/Debug/net10.0/Template.AssemblyInfo.cs
+++ Template/obj/Debug/net10.0/Template.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Template")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0423ba4d2d337ede4e3e54ee8ca03fd43a441178")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+316d49b69493b00b0eadf864af8b68f303aaa173")]
 [assembly: System.Reflection.AssemblyProductAttribute("Template")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Template")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
