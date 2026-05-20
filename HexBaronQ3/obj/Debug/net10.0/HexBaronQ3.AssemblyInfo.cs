6e456f6a50a4370a7abc4b590d1654872a015395
Yui <Pielord.home@gmail.com>
2026-05-20 17:20:45 +0100

    q4 and 5

--- HexBaronQ3/obj/Debug/net10.0/HexBaronQ3.AssemblyInfo.cs
+++ HexBaronQ3/obj/Debug/net10.0/HexBaronQ3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HexBaronQ3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+316d49b69493b00b0eadf864af8b68f303aaa173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42c07bac1f4bd1a76f0bd8a32da1d543dfa0a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("HexBaronQ3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HexBaronQ3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
