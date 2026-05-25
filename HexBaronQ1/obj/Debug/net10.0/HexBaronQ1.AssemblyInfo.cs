6adc48956c6523a55c5e8a7679befa2b076d1f4a
Yui <pielord.home@gmail.com>
2026-05-25 17:31:46 +0100

    Add HexBaronQ7 project and update SDK to 10.0.300
    Added new HexBaronQ7 project with full game implementation and updated solution/workspace metadata. Upgraded all projects to .NET SDK 10.0.300, updated assembly info and NuGet cache files, and cleaned up obsolete metadata files.

--- HexBaronQ1/obj/Debug/net10.0/HexBaronQ1.AssemblyInfo.cs
+++ HexBaronQ1/obj/Debug/net10.0/HexBaronQ1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HexBaronQ1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e456f6a50a4370a7abc4b590d1654872a015395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75d87dfb46c7bce7781908163af2c0158b79de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HexBaronQ1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HexBaronQ1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
